a475eb995c44a4b93c97538b3a94931ad192fba2
Maik Krawinkel <maikkrawinkel@gmail.com>
2025-12-31 08:31:48 +0100

    Daten ausgelagert.

---
+++ data.js
@@ -0,0 +1,14 @@
+const entries = [
+  {
+    id: "serendipitaet",
+    word: "Serendipität",
+    language: "Deutsch",
+    definition:
+      "Das glückliche Finden von etwas Wertvollem oder Interessantem, nach dem man gar nicht gezielt gesucht hat.",
+    examples: [
+      "Durch einen Zufall stieß er auf das perfekte Buch – reine Serendipität.",
+      "Viele wissenschaftliche Entdeckungen beruhen auf Serendipität.",
+      "Beim Stöbern im Antiquariat erlebt sie oft Momente der Serendipität.",
+    ],
+  },
+];
